bd60ca5b92930870ef85340687c8f583e3256a30
Luciano Ricardo <lucianoricardo737@gmail.com>
2023-05-26 18:48:16 -0300

    fix infobae fetch

--- src/context/env.js
+++ src/context/env.js
@@ -1,3 +1,5 @@
 export const webDolarHoy = "https://www.dolarhoy.com/cotizaciondolarblue"
 
-export const webInfobae = "https://www.infobae.com/economia"
+export const webInfobae = "https://www.infobae.com/economia/"
+
+export const apiInfobae = "https://currencies-data.infobae.com/byMinute"
--- src/routes/infobae_routes.js
+++ src/routes/infobae_routes.js
@@ -1,113 +1,127 @@
 import { Router } from 'express';
 import cheerio from 'cheerio';
-import { webInfobae } from '../context/env';
-import fetchData from '../context/fetchData';
+import { apiInfobae } from '../context/env';
 
 const router = Router();
 
 router.get('/', async (req, res) => {
-    try {
 
-        const body = await fetchData(webInfobae)
+    async function fetchData() {
+        try {
+            const response = await fetch(apiInfobae, {
+                method: 'GET',
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0',
+                    'Accept': 'application/json, text/plain, */*',
+                    'Accept-Language': 'es-AR,es;q=0.8,en-US;q=0.5,en;q=0.3',
+                    'Accept-Encoding': 'gzip, deflate, br',
+                    'Origin': 'https://www.infobae.com',
+                    'Referer': 'https://www.infobae.com/',
+                    'Sec-Fetch-Dest': 'empty',
+                    'Sec-Fetch-Mode': 'cors',
+                    'Sec-Fetch-Site': 'same-site',
+                    'Pragma': 'no-cache',
+                    'Cache-Control': 'no-cache'
+                }
+            });
 
-        const $ = cheerio.load(body);
-        const dolarMore = $('.d23-excbar')
-        
-
-        let nacion,
-            tarjeta,
-            libre,
-            mep,
-            liqui,
-            rp
-
-
-        const filters = {
-            nacion: {
-                text: 'Dólar Banco Nación',
-                href: "https://www.infobae.com/tag/dolar-banco-nacion/"
-            },
-            tarjeta: {
-                text: 'Dólar Turista',
-                href: "https://www.infobae.com/tag/dolar-turista/"
-            },
-            libre: {
-                text: 'Dólar Libre',
-                href: "https://www.infobae.com/tag/dolar-hoy/"
-            },
-            mep: {
-                text: 'Dólar MEP',
-                href: "https://www.infobae.com/tag/dolar-bolsa/"
-            },
-            liqui: {
-                text: 'Contado con liqui',
-                href: "https://www.infobae.com/tag/contado-con-liqui/"
-            },
-            rp: {
-                text: 'Riesgo País',
-                href: "https://www.infobae.com/tag/riesgo-pais/"
-            },
-        }
-
-
-        dolarMore.find('a').map((i, link) => {
-            
-            const key = link.attribs.href.toLowerCase()
-            let data = $(link).text()
-            // console.log(data)
-
-
-            switch (key) {
-                case filters.nacion.href:
-                    data = data.split(filters.nacion.text)[1]
-                    nacion = data
-                    break;
-                case filters.tarjeta.href:
-                    data = data.split(filters.tarjeta.text)[1]
-                    tarjeta = data
-                    break;
-                case filters.libre.href:
-                    data = data.split(filters.libre.text)[1]
-                    libre = data
-                    break;
-                case filters.mep.href:
-                    data = data.split(filters.mep.text)[1]
-                    mep = data
-                    break;
-                case filters.liqui.href:
-                    data = data.split(filters.liqui.text)[1]
-                    liqui = data
-                    break;
-                case filters.rp.href:
-                    data = data.split(filters.rp.text)[1]
-                    rp = data
-                    break;
-                default: data
-                    break;
+            if (response.ok) {
+                const data = await response.json();
+                return data
+            } else {
+                console.log('La solicitud no fue exitosa. Código de estado:', response.status);
             }
-
-            return data
-        })
-
-
-        function parse(data) {
-            return parseFloat(parseFloat(data).toFixed(2))
+        } catch (error) {
+            console.log('Error al realizar la solicitud:', error.message);
         }
-
-        const data = {
-            nacion: parse(nacion),
-            tarjeta: parse(tarjeta),
-            libre: parse(libre),
-            mep: parse(mep),
-            liqui: parse(liqui),
-            rp: parse(rp)
-        }
-
-        console.log(data)
-        res.status(200).json(data)
-    } catch (error) {
-        res.status(400).json({ msg: "error feching dolar from infobae" })
     }
+
+    const dolarValues = await fetchData();
+
+    const filters = {
+        nacion: {
+            ric: 'ARS=BNAR',
+            name: 'Dólar Banco Nación'
+        },
+        tarjeta: {
+            ric: 'ARST=',
+            name: 'Dólar Turista'
+        },
+        libre: {
+            ric: 'ARSB=',
+            name: 'Dólar Libre'
+        },
+        mep: {
+            ric: 'ARSCONT0=',
+            name: 'Contado con Liqui'
+        },
+        liqui: {
+            ric: 'ARSMEP0=',
+            name: 'Dólar MEP'
+        }
+    }
+
+    const filteredData = Object.values(filters).map(filter => {
+        return dolarValues.find(item => item.ric === filter.ric);
+    });
+
+    let dolarReturn = {
+        nacion: {
+            venta: 0,
+            compra: 0,
+            name: filters.nacion.name
+        },
+        tarjeta: {
+            venta: 0,
+            compra: 0,
+            name: filters.tarjeta.name
+        },
+        libre: {
+            venta: 0,
+            compra: 0,
+            name: filters.libre.name
+        },
+        mep: {
+            venta: 0,
+            compra: 0,
+            name: filters.mep.name
+        },
+        liqui: {
+            venta: 0,
+            compra: 0,
+            name: filters.liqui.name
+        }
+    }
+
+    filteredData.forEach(item => {
+        switch (item.ric) {
+            case 'ARS=BNAR':
+                dolarReturn.nacion.venta = item.sellValue;
+                dolarReturn.nacion.compra = item.buyValue;
+                break;
+            case 'ARST=':
+                dolarReturn.tarjeta.venta = item.sellValue; 
+                dolarReturn.tarjeta.compra = item.buyValue;
+                break;
+            case 'ARSB=':
+                dolarReturn.libre.venta = item.sellValue;
+                dolarReturn.libre.compra = item.buyValue;
+                break;
+            case 'ARSCONT0=':
+                dolarReturn.mep.venta = item.sellValue;
+                dolarReturn.mep.compra = item.buyValue;
+                break;
+            case 'ARSMEP0=':
+                dolarReturn.liqui.venta = item.sellValue;
+                dolarReturn.liqui.compra = item.buyValue;
+                break;
+            default:
+                break;
+        }
+    });
+
+    res.status(200).json(dolarReturn)
+
 })
 
 
